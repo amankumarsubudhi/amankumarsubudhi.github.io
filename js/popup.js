a8456708c926e08bd70abeef57676ccdad201f45
amankumarsubudhi <aman.work404@gmail.com>
2024-05-26 18:39:00 +0530

    initial commit

---
+++ js/popup.js
@@ -0,0 +1,5 @@
+function closePopup(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup-overlay").style.display = "none";
+    document.querySelector("body").style.overflowY="scroll";
+}
