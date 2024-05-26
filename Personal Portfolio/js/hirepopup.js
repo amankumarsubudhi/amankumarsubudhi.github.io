6870699e8447b6d53631b3726413bd88aaebc757
Aman Kumar Subudhi <147010957+amankumarsubudhi@users.noreply.github.com>
2024-05-26 18:19:04 +0530

    Delete Personal Portfolio directory

--- Personal Portfolio/js/hirepopup.js
+++
@@ -1,9 +0,0 @@
-function hireClick(){
-    document.querySelector(".hire-overlay").style.visibility="visible";
-    document.querySelector(".hire-popup").style.visibility="visible";
-}
-
-function hireCloseClick(){
-    document.querySelector(".hire-overlay").style.visibility="hidden";
-    document.querySelector(".hire-popup").style.visibility="hidden";
-}
